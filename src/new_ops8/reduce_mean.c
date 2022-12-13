538d85b9c77dc331ecf8e5f8c22b2aae47d5b934
xgy <995975102@qq.com>
2022-12-12 19:17:27 -0800

    add: reduce_mean op and unit test.

---
+++ src/new_ops8/reduce_mean.c
@@ -0,0 +1,31 @@
+#include "src/new_ops8/reduce_mean.h"
+#include "src/core/allocator.h"
+#include "src/core/dispatch.h"
+#include "src/new_ops8/reduce_sum.h"
+
+#define reduce_mean_kernel(typename, dims, dims_length, num_items) \
+  typename* in_data = (typename*)aitisa_tensor_data(input);        \
+  double* out_data = (double*)aitisa_tensor_data(*output);         \
+  int64_t* input_dims = aitisa_tensor_dims(input);                 \
+  double factor = 1;                                               \
+  for (int64_t i = 0; i < dims_length; i++) {                      \
+    factor *= input_dims[dims[i]];                                 \
+  }                                                                \
+                                                                   \
+  for (int i = 0; i < num_items; i++) {                            \
+    out_data[i] /= factor;                                         \
+  }
+
+Status aitisa_reduce_mean(const Tensor input, const int64_t* dims,
+                          const int64_t dims_length, const int keepdim,
+                          Tensor* output) {
+
+  Status status = STATUS_SUCCESS;
+  DataType prods_dtype = aitisa_tensor_data_type(input);
+  status = aitisa_reduce_sum(input, dims, dims_length, keepdim, output);
+  int64_t num_items = aitisa_tensor_size(input);
+  AITISA_DISPATCH_ALL_TYPES_RETURN(prods_dtype, reduce_mean_kernel, dims,
+                                   dims_length, num_items);
+
+  return status;
+}
--- src/new_ops8/reduce_mean.h
+++ src/new_ops8/reduce_mean.h
@@ -1,9 +1,9 @@
-#ifndef REDUCE_PROD
-#define REDUCE_PROD
+#ifndef REDUCE_MEAN
+#define REDUCE_MEAN
 
 #include "src/core/tensor.h"
 /**
- * @brief Applies a reduce_prod over an input tensor.
+ * @brief Applies a reduce_mean over an input tensor.
  *
  * @param input The input tensor.
  * @param output The output tensor pointer.
@@ -11,9 +11,9 @@
  * @param keepdim whether the output tensor has dim retained or not.
  * @return Status The Status enum indicates whether the routine is OK.
  */
-AITISA_API_PUBLIC Status aitisa_reduce_prod(const Tensor input,
+AITISA_API_PUBLIC Status aitisa_reduce_mean(const Tensor input,
                                             const int64_t* dims,
                                             const int64_t dims_length,
                                             const int keepdim, Tensor* output);
 
-#endif  // REDUCE_PROD
+#endif  // REDUCE_MEAN
